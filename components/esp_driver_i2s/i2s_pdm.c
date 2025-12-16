c49a47369ef00e58b49b1191c37d76450a419322
Marc CAPDEVILLE <m.capdeville@no-log.org>
2025-12-16 19:01:35 +0100

    fix(esp_pdm): fix warning in esp_pdm.c
    Suppress warning message when PDM_CLOCK_WORKAUROUND is disabled.

--- components/esp_driver_i2s/i2s_pdm.c
+++ components/esp_driver_i2s/i2s_pdm.c
@@ -92,8 +92,6 @@ static esp_err_t i2s_pdm_tx_set_clock(i2s_chan_handle_t handle, const i2s_pdm_tx
     /* Work around for PDM TX clock, overwrite the raw division directly to reduce the noise
      * This set of coefficients is a special division to reduce the background noise in PDM TX mode */
     i2s_ll_tx_set_raw_clk_div(handle->controller->hal.dev, clk_info.mclk_div, 1, 1, 0, 0);
-#else
-#warning "I2S PDM clock workaround disabled"
 #endif
     portEXIT_CRITICAL(&g_i2s.spinlock);
 
